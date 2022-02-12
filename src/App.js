d2ea1dfd3dc20681fdeed5760b8fba92c47cae0c
kundurthivenkatakishore <kundurthivenkatakishore@gmail.com>
2022-02-12 18:44:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import Plans from "./Plans";
+import "./App.css";
 
 function App() {
+  const plans = [
+    {
+      name: "Free",
+      price: 0,
+      features: [
+        { name: "Single User", enabled: true },
+        { name: "5GB Storage", enabled: true },
+        { name: "Unlimited Public Projects", enabled: true },
+        { name: "Community Access", enabled: true },
+        { name: "Unlimited Private Projects", enabled: false },
+        { name: "Dedicated Phone Support", enabled: false },
+        { name: "Free Subdomain", enabled: false },
+        { name: "Monthly Status Reports", enabled: false }
+      ],
+    },
+    {
+      name: "Plus",
+      price: 9,
+      features: [
+        { name: "Single User", enabled: true },
+        { name: "5GB Storage", enabled: true },
+        { name: "Unlimited Public Projects", enabled: true },
+        { name: "Community Access", enabled: true },
+        { name: "Unlimited Private Projects", enabled: true },
+        { name: "Dedicated Phone Support", enabled: true },
+        { name: "Free Subdomain", enabled: true },
+        { name: "Monthly Status Reports", enabled: false }
+      ],
+    },
+    {
+      name: "pro",
+      price: 49,
+      features: [
+        { name: "Single User", enabled: true },
+        { name: "5GB Storage", enabled: true },
+        { name: "Unlimited Public Projects", enabled: true },
+        { name: "Community Access", enabled: true },
+        { name: "Unlimited Private Projects", enabled: true },
+        { name: "Dedicated Phone Support", enabled: true },
+        { name: "Free Subdomain", enabled: true },
+        { name: "Monthly Status Reports", enabled: true }
+      ],
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="pricing py-5">
+      <div className="container">
+        <div className="row">
+          {
+            plans.map((plan, index) => {
+              return <Plans key={index} name={plan.name} price={plan.price} features={plan.features} />
+            })
+          }
+        </div>
+      </div>
+    </section>
   );
 }
 
